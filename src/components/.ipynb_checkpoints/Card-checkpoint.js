c1ecd1d02ef29cd9bc27cc4e09e257e21bff4539
Ubuntu <ubuntu@ip-172-31-5-153.us-east-2.compute.internal>
2020-08-18 12:28:39 +0000

    update frontend

---
+++ src/components/.ipynb_checkpoints/Card-checkpoint.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CardClass from './CardClass'
+
+// The Roster component matches one of two different routes
+// depending on the full pathname
+const Card = () => (
+    <CardClass />
+);
+
+export default Card;
